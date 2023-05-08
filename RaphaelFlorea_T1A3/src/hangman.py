89aa489370c2a3f1104e5fb5e0ff70449b616b4b
ausraf720 <65544863+ausraf720@users.noreply.github.com>
2023-05-08 20:40:05 +1000

    Added all hangman animations
    All 10 forms of hangman plus blank hangman added, including function to streamline the process.

--- RaphaelFlorea_T1A3/src/hangman.py
+++ RaphaelFlorea_T1A3/src/hangman.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 
-#Make the hangman class, which is template for hangman stages to be printed out
+#Make the hangman class, 
+#which is template for hangman stages to be printed out
 class hangman_stage:
     def __init__(self, lines):
         self.lines = lines
@@ -14,10 +15,19 @@ class hangman_stage:
     def modify_line(self, line_index, new_line):
         self.lines[line_index] = new_line
 
+    #This builds on previous function
+    #It deep-copies the previous hangman stage,
+    #then modifies line of next stage and adds it to list of stages
+    def stage_adder(self, stages, line_index, new_line):
+        new_stage = deepcopy(self)
+        new_stage.modify_line(line_index, new_line)
+        stages.append(new_stage)
+        return new_stage
+
 #/****************************************************************/
 
 
-#Here, make the full hangman picture to be printed out, and gameover
+#Here, make the full hangman picture to be printed out
 lines = []
 lines.append('_______ ')
 lines.append('|    |  ')
@@ -25,30 +35,39 @@ lines.append('|    O  ')
 lines.append('|   -|- ')
 lines.append('|   / \ ')
 lines.append('|_______')
-stage_final = hangman_stage(lines)
-stages = []
+stage10 = hangman_stage(lines)
 
 #Make a list of all the hangman stages
 #Note, the list will be reversed at the end,
 #as appending stages in backwards order
-stages.append(stage_final)
-old_stage = stage_final
+
+stages = []
+stages.append(stage10)
 
 #/****************************************************************/
 
-#Now start adding all the other stages
-#Need to build a function to avoid repeating code later
+#Now start adding all the other stages, using 'stage_adder'
+stage9 = stage10.stage_adder(stages, 4, '|   /   ')
+stage8 = stage9.stage_adder(stages, 4, '|       ')
+stage7 = stage8.stage_adder(stages, 3, '|   -|  ')
+stage6 = stage7.stage_adder(stages, 3, '|    |  ')
+stage5 = stage6.stage_adder(stages, 3, '|       ')
+stage4 = stage5.stage_adder(stages, 2, '|       ')
+stage3 = stage4.stage_adder(stages, 1, '|       ')
+stage2 = stage3.stage_adder(stages, 0, '        ')
 
-new_stage = deepcopy(old_stage)
-new_stage.modify_line(4, '|   /   ')
-stages.append(new_stage)
-old_stage = new_stage
+#stage1 needs to remove vertical left column of hangman picture,
+#Thus, call modify_line function directly to remove from each line
 
-new_stage = deepcopy(old_stage)
-new_stage.modify_line(4, '|       ')
-stages.append(new_stage)
-old_stage = new_stage
+stage1 = deepcopy(stage2)
+for i in range(1,5):
+    stage1.modify_line(i, '        ')
+stage1.modify_line(5, ' _______')
+stages.append(stage1)
 
-#Do initial tests
+#Finally create initial blank stage
+stage0 = stage1.stage_adder(stages, 5, '        ')
+
+#Test all stages are printing out correctly
 for i in stages:
     i.printer()
