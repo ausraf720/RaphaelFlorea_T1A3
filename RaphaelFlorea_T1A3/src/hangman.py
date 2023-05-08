7175c9b01e0143e9321583abf56b62dd66f50b42
ausraf720 <65544863+ausraf720@users.noreply.github.com>
2023-05-08 18:42:42 +1000

    Added more functionality to hangman class
    Added new method so now able to modify individual lines of printed-out hangman. However, run into problem where copying instance of class changes original instance, so need to solve this.

--- RaphaelFlorea_T1A3/src/hangman.py
+++ RaphaelFlorea_T1A3/src/hangman.py
@@ -4,9 +4,14 @@ class hangman_stage:
     def __init__(self, lines):
         self.lines = lines
 
+    #Prints everything out
     def printer(self):
         for i in self.lines:
             print(i)
+
+    #Modifies individual line of hangman output
+    def modify_line(self, line_index, new_line):
+        self.lines[line_index] = new_line
 
 
 
@@ -19,6 +24,12 @@ lines.append('|   -|- ')
 lines.append('|   / \ ')
 lines.append('|_______')
 
+#Test all of above code is working
 stage10 = hangman_stage(lines)
+stage9 = stage10
+stage9.modify_line(4, '|   /   ')
 
+#Testing to see how by copying stage9 from stage10,
+#stage10 now produces same result as stage9
+stage9.printer()
 stage10.printer()
--- RaphaelFlorea_T1A3/src/main.py
+++ RaphaelFlorea_T1A3/src/main.py
@@ -10,21 +10,25 @@ word_list = words.split("\n")
 #remove last word as it's just blank /n character
 del(word_list[-1])
 
+#/****************************************************************/
+
 #This section of code used to sort words into lists,
 #depending on their word length
 
-max = 0
+max_word_len = 0
 for i in word_list:
-    if len(i) > max:
-        max = len(i)
+    if len(i) > max_word_len:
+        max_word_len = len(i)
 
+#grand list is a 2d list, 
+#each list contains words only of a certain length
 grand_list = []
-for i in range(max):
+for i in range(max_word_len):
     grand_list.append([])
 
 for i in word_list:
     grand_list[len(i)-1].append(i)
 
-print(grand_list[17])
-
 #/****************************************************************/
+
+print(grand_list[5])
